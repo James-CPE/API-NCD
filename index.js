9312c65f826e765a1501772d8466a63f15f41498
James Rodriguez <ihegb.ok@gmail.com>
2025-08-18 10:52:50 +0700

    update api

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2/promise');
 const cors = require('cors');
-const { status } = require('express/lib/response');
 
 const app = express();
 app.use(cors());
